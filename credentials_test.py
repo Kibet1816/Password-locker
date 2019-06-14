52545c575133beb9b2b3b488e6493df6cd0a4e76
Denis Kibet <kibet1816@gmail.com>
2019-06-14 12:24:34 +0300

    Run test to save multiple credentials

--- credentials_test.py
+++ credentials_test.py
@@ -11,6 +11,12 @@ class TestCredentials(unittest.TestCase):
         """
         self.new_credentials = Credentials("Github","Kibet1816","@#soccerkibe1816")
 
+    def tearDown(self):
+        """
+        Method that cleans up after each test
+        """
+        Credentials.credentials_list = []
+
     def test_init(self):
         """
         Test for correct initialization
@@ -26,6 +32,14 @@ class TestCredentials(unittest.TestCase):
         self.new_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list),1)
 
+    def test_save_multiple_credentials(self):
+        """
+        Test for saving multiple credentials
+        """
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("AllFootball","Kibet","messithegoat")
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)
 
 if __name__ == '__main__':
     unittest.main()
