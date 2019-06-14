95d1fea89c1a05111b7760420c30f2d523758f3c
Denis Kibet <kibet1816@gmail.com>
2019-06-14 11:29:14 +0300

    Add user and credential classes

---
+++ password.py
@@ -0,0 +1,10 @@
+class User:
+    """
+    Class to define login details for the user in the application
+    """
+
+
+class Credentials:
+    """
+    Class to define credentials for the different accounts
+    """
