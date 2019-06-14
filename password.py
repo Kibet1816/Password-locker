60d16488192cbd68901f0a9c49323c179db3807e
Denis Kibet <kibet1816@gmail.com>
2019-06-14 11:33:42 +0300

    Define instances for both classes

--- password.py
+++ password.py
@@ -3,9 +3,15 @@ class User:
     Class to define login details for the user in the application
     """
 
+    def __init__(self,first_name,last_name,password):
+        pass
+
 
 class Credentials:
     """
     Class to define credentials for the different accounts
     """
     credentials_list = []
+
+    def __init__(self,account_name,username,password):
+        pass
