ba3e74061c7c7b68c3055e6c248602cb9e3b03fa
Denis Kibet <kibet1816@gmail.com>
2019-06-14 15:10:35 +0300

    Create new function  to add a user login

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3.6
 from password import User
 from password import Credentials
+
+def create_login(firstname,lastname,password):
+    """
+    Function to create a new login
+    """
+    new_user = User(firstname,lastname,password)
+    return new_user
+
