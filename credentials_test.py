05eca76a3edc0a892db4dcd7c25527e6645795b0
Denis Kibet <kibet1816@gmail.com>
2019-06-14 11:52:17 +0300

    Run test for correct initialization

--- credentials_test.py
+++ credentials_test.py
@@ -6,4 +6,19 @@ class TestCredentials(unittest.TestCase):
     Class to test behaviour of the credentials class
     """
     def setUp(self):
+        """
+        Setup method that defines instructions
+        """
         self.new_credentials = Credentials("Github","Kibet1816","@#soccerkibe1816")
+
+    def test_init(self):
+        """
+        Test for correct initialization
+        """
+        self.assertEqual(self.new_credentials.account_name,"Github")
+        self.assertEqual(self.new_credentials.username,"Kibet1816")
+        self.assertEqual(self.new_credentials.password,"@#soccerkibe1816")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- user_test.py
+++ user_test.py
@@ -6,4 +6,19 @@ class TestUser(unittest.TestCase):
     Class for testing the behaviour of the user class
     """
     def setUp(self):
+        """
+        Setup method that defines instructions
+        """
         self.new_user = User("Denis","Kibet","soccer5240")
+
+    def test_init(self):
+        """
+        Test for correct initialization
+        """
+        self.assertEqual(self.new_user.first_name,"Denis")
+        self.assertEqual(self.new_user.last_name,"Kibet")
+        self.assertEqual(self.new_user.password,"soccer5240")
+
+
+if __name__ == '__main__':
+    unittest.main()
