aac5d0b5490c365f226f01018b072dabfe44dcd5
Denis Kibet <kibet1816@gmail.com>
2019-06-14 15:13:31 +0300

    Add new function to add a new account

--- run.py
+++ run.py
@@ -9,3 +9,10 @@ def create_login(firstname,lastname,password):
     new_user = User(firstname,lastname,password)
     return new_user
 
+def add_account(accname,username,password):
+    """
+    Function to add a new account and its credentials
+    """
+    new_credentials = Credentials(accname,username,password)
+    return new_credentials
+
