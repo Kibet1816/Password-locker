f3ba74c2d3202ca00e2b9250e0b66aa74f1394f4
Denis Kibet <kibet1816@gmail.com>
2019-06-14 11:40:19 +0300

    Create subclasses in the test files

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,7 @@
 import unittest
 from password import Credentials
+
+class TestCredentials(unittest.TestCase):
+    """
+    Class to test behaviour of the credentials class
+    """
--- user_test.py
+++ user_test.py
@@ -1,2 +1,7 @@
 import unittest
 from password import User
+
+class TestUser(unittest.TestCase):
+    """
+    Class for testing the behaviour of the user class
+    """
